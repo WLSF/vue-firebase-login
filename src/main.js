6c5aeaebfed5405d0f351e0cb49d31abf862eeee
Willian Frantz <willianluigii@gmail.com>
2019-02-09 19:32:29 -0300

    :art: Add env vars to Firebase

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import firebase from 'firebase'
 Vue.config.productionTip = false
 
 var config = {
-  apiKey: "AIzaSyB6zov2BGx8CTpuNfBfVpPLrfPVc5_on4o",
-  authDomain: "vuejs-workshop-30016.firebaseapp.com",
-  databaseURL: "https://vuejs-workshop-30016.firebaseio.com",
-  projectId: "vuejs-workshop-30016",
-  storageBucket: "vuejs-workshop-30016.appspot.com",
-  messagingSenderId: "854305684478"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_ID
 };
 firebase.initializeApp(config);
 
