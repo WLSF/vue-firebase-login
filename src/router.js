96c45c763f3ee424aadbd348562474d7b62b8939
Willian Frantz <willianluigii@hotmail.com>
2019-02-09 19:38:35 -0300

    :art: Change lazy load import on About.vue

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Signup from './views/Signup.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -28,10 +29,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     }
   ]
 })
