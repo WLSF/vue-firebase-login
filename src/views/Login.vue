2fe909f483b555b2131211ab0e975ef75dd39824
Willian Frantz <willianluigii@hotmail.com>
2019-02-11 01:54:05 -0300

    :lipstick: Add new vuetify template on Signup and Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,33 @@
 <template>
-  <form>
-    <v-text-field
-      v-model="email"
-      label="Email"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      :error-messages="errors"
-      label="Password"
-      required
-    ></v-text-field>
-
-    <v-btn @click="login">Login</v-btn>
-    <router-link to="signup">Signup</router-link>
-  </form>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-form>
+          <v-card-text>
+            <v-text-field
+              v-model="email"
+              :rules="[rules.required, rules.email]"
+              label="Email"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              :append-icon="show ? 'visibility_off' : 'visibility'"
+              :rules="[rules.required, rules.min]"
+              :type="show ? 'text' : 'password'"
+              :error-messages="errors"
+              label="Password"
+              hint="At least 8 chars"
+              @click:append="show = !show"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="login">Login</v-btn>
+            <router-link to="/signup">Signup</router-link>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import firebase from 'firebase'
@@ -24,7 +37,16 @@ export default {
     return {
       email: "",
       password: "",
-      errors: ""
+      errors: "",
+      rules: {
+        required: value => !!value || 'Required!',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid email.'
+        },
+        min: value => value.length >= 8 || 'Min 8 chars'
+      },
+      show: false,
     }
   },
   methods: {
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,13 +1,33 @@
 <template>
-  <div class="signup">
-    <input type="text" v-model="email" name="email">
-    <input type="password" v-model="password" name="password">
-
-    <button @click='createUser'>Criar conta</button>
-    <router-link to="/login"><p>Login</p></router-link>
-
-    <p v-if="errors">{{ errors }}</p>
-  </div>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-form>
+          <v-card-text>
+            <v-text-field
+              v-model="email"
+              :rules="[rules.required, rules.email]"
+              label="Email"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              :append-icon="show ? 'visibility_off' : 'visibility'"
+              :rules="[rules.required, rules.min]"
+              :type="show ? 'text' : 'password'"
+              :error-messages="errors"
+              label="Password"
+              hint="At least 8 chars"
+              @click:append="show = !show"
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="signup">Signup</v-btn>
+            <router-link to="/login">Login</router-link>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import firebase from 'firebase'
@@ -17,15 +37,24 @@ export default {
     return {
       email: "",
       password: "",
-      errors: ""
+      errors: "",
+      rules: {
+        required: value => !!value || 'Required!',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid email.'
+        },
+        min: value => value.length >= 8 || 'Min 8 chars'
+      },
+      show: false,
     }
   },
   methods: {
-    createUser: function() {
+    signup: function() {
       firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(() => {
         this.$router.push({ name: 'home' })
       }).catch((error) => {
-        this.errors = error
+        this.errors = error.message
       })
     }
   }
